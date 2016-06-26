c4637bf05b6292dd9314647e1fa0d6a7bac68545
Hoan Nguyen <nguyenhoan.hut@gmail.com>
2016-06-26 09:39:04 +0700

    Fan

---
+++ Fan.java
@@ -0,0 +1,64 @@
+package fan;
+
+public class Fan {
+	final int SLOW = 1;
+	final int MEDIUM = 2;
+	final int FAST = 3;
+	private int speed = SLOW;
+	boolean on = false;
+	private double radius = 5;
+	String color = "blue";
+
+	public Fan(int speed, boolean on, double radius, String color) {
+		super();
+		this.speed = speed;
+		this.on = on;
+		this.radius = radius;
+		this.color = color;
+	}
+
+	public int getSpeed() {
+		return speed;
+	}
+
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
+
+	public boolean isOn() {
+		return on;
+	}
+
+	public void setOn(boolean on) {
+		this.on = on;
+	}
+
+	public double getRadius() {
+		return radius;
+	}
+
+	public void setRadius(double radius) {
+		this.radius = radius;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public Fan() {
+		super();
+	}
+
+	public String toString() {
+		if (on == true) {
+			return "fan color " + color + " radius " + radius;
+		} else {
+			return "fan is off";
+		}
+	}
+
+}
